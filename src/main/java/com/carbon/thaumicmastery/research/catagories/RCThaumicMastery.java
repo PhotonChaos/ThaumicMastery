6a34f641cb949f77bdee02ebec635e4e246d7802
PhotonChaos <calumgcoppack@gmail.com>
2018-06-23 01:00:29 -0600

    Added background

---
+++ src/main/java/com/carbon/thaumicmastery/research/catagories/RCThaumicMastery.java
@@ -0,0 +1,16 @@
+package com.carbon.thaumicmastery.research.catagories;
+
+import com.carbon.thaumicmastery.ThaumicMastery;
+import com.thaumcraft.research.ResearchCategoryList;
+import net.minecraft.util.ResourceLocation;
+
+public class RCThaumicMastery extends ResearchCategoryList {
+	public ResourceLocation tabIcon = new ResourceLocation(ThaumicMastery.MODID, "textures/research/category/thaumicmastery.png");
+
+
+	public RCThaumicMastery(ResourceLocation icon, ResourceLocation background) {
+		super(icon, background);
+	}
+
+
+}
