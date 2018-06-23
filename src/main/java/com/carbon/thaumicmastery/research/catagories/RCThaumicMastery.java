a12c48cf3ffbf9d300bb57c9819e7a32fcc12f30
PhotonChaos <calumgcoppack@gmail.com>
2018-06-23 01:04:29 -0600

    more code

--- src/main/java/com/carbon/thaumicmastery/research/catagories/RCThaumicMastery.java
+++ src/main/java/com/carbon/thaumicmastery/research/catagories/RCThaumicMastery.java
@@ -6,7 +6,7 @@ import net.minecraft.util.ResourceLocation;
 
 public class RCThaumicMastery extends ResearchCategoryList {
 	public ResourceLocation tabIcon = new ResourceLocation(ThaumicMastery.MODID, "textures/research/category/thaumicmastery.png");
-
+	public ResourceLocation bg = new ResourceLocation(ThaumicMastery.MODID, "textures/research/category/bg.png");
 
 	public RCThaumicMastery(ResourceLocation icon, ResourceLocation background) {
 		super(icon, background);
