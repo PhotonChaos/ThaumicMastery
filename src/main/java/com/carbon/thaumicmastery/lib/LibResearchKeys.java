b5301b7f5532779c196b3f1bfa8e374586b2263e
PhotonChaos <calumgcoppack@gmail.com>
2018-06-25 07:42:04 -0600

    Added libs

---
+++ src/main/java/com/carbon/thaumicmastery/lib/LibResearchKeys.java
@@ -0,0 +1,13 @@
+package com.carbon.thaumicmastery.lib;
+
+public class LibResearchKeys {
+	public static final String CATAGORY_NAME = "TM_CATEGORY";
+
+	public static final String KEY_ROOT = "TM_root";
+	public static final String KEY_AIR  = "TM_air";
+	public static final String KEY_ORDER = "TM_order";
+	public static final String KEY_ENTROPY = "";
+	public static final String KEY_FIRE = "";
+	public static final String KEY_EARTH = "";
+
+}
